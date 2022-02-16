9259dc28193224f2caad37788bdd7b56e45a8f71
Saheb Giri <iamsahebgiri@gmail.com>
2022-02-17 03:01:02 +0530

    chore: :lipstick: prevent user selection on button

--- components/icon-button.tsx
+++ components/icon-button.tsx
@@ -4,7 +4,7 @@ export default function IconButton(props) {
   return (
     <button
       type="button"
-      className="py-2 px-4 text-sm font-medium text-slate-900 bg-white rounded-full inline-flex items-center hover:bg-slate-50 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-0 focus-visible:ring-blue-200"
+      className="py-2 px-4 text-sm select-none font-medium text-slate-900 bg-white rounded-full inline-flex items-center hover:bg-slate-50 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-0 focus-visible:ring-blue-200"
       {...props}
     />
   );
